08503e4fba3f53176f74eec491c325896c94f058
seiji <ymst500@gmail.com>
2018-08-15 15:58:47 +0900

    Sidebar newest message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :name, presence: true
+
+  def show_newest_message
+    if (newest_message = messages.last ).present?
+      newest_message.body? ? newest_message.body : "画像が投稿されています"
+    else
+      "メッセージはまだありません"
+    end
+  end
 end
