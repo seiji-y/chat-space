eb352aca49df9968e4d9614810077120304cb719
seiji <ymst500@gmail.com>
2018-08-16 15:23:08 +0900

    Ready for factory girl

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :user do
+    name Faker::Name.last_name
+    email Faker::Internet.free_email
+    password = Faker::Internet.password(8)
+    password password
+    password_confirmation password
+  end
+
+end
